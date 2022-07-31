550b8ee00b5f06519f78df32b80f944f194c6aa2
Aryan <aryanrsuri@gmail.com>
2022-07-31 10:36:44 -0700

    working RPS with async functions (maybe need debugging)

--- src/main.js
+++ src/main.js
@@ -1,30 +1,48 @@
 const options = ["rock", "paper", "scissors"];
+const box = document.getElementById("resp");
 async function game() {
-    for (let i=0; i < 5; i++) {
         try {
-            round();
-        } catch(Error) {
-            console.error(Error);
+            let resp = await round()
+            console.log(resp)
+            box.innerHTML = resp;
+        } catch(Err) {
+            console.error(Err);
         }
-
-    }
 }
 
-async function computerResponse() {
+function computerResponse() {
     let ran = Math.floor(Math.random() * 4);
 
     return options[ran];
 }
 
 async function playerResponse() {
-    let resp = prompt("Rock, paper, or scissors?: \n");
+    let resp = prompt("Rock, paper, or scissors?: ");
 
-    return resp;
+    return resp.trim().toLowerCase();
 }
 
-async function round(computerResponse, playerResponse) {
-    let CPU = await computerResponse();
-    let PRU = await playerResponse();
+async function round() {
+    const CR = computerResponse();
+    const PR = await playerResponse();
+    console.log(CR,PR);
+    if (CR === PR) {
+        return Promise.resolve("It's a tie!")
+    }
+    const DISCR = CR+PR;
+    switch(DISCR) {
+        case "rockscissors":
+        case "paperrock":
+        case "scissorspaper":
+            return Promise.resolve(`Computer wins with the move: ${CR} \nyour move was ${PR}`)
+        case "scissorsrock":
+        case "rockpaper":
+        case "paperscissors":
+            return `You win with the move: ${PR} !`
+        default:
+            return Promise.reject(Error)
+    }
+
 }
 
 async function test() {
@@ -38,4 +56,6 @@ async function test() {
     console.log("test called");
 }
 
-(async () => {await test();})();
+(async () => {
+    await game();
+})();
