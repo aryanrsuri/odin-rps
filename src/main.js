acdb3fcb07c1d3ae83a0d1c57f95b8a76263e84d
Aryan <aryanrsuri@gmail.com>
2022-07-31 09:27:34 -0700

    js-updates

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 const options = ["rock", "paper", "scissors"];
 async function game() {
+    for (let i=0; i < 5; i++) {
+        try {
+            round();
+        } catch(Error) {
+            console.error(Error);
+        }
 
+    }
 }
 
 async function computerResponse() {
@@ -18,7 +25,17 @@ async function playerResponse() {
 async function round(computerResponse, playerResponse) {
     let CPU = await computerResponse();
     let PRU = await playerResponse();
-    switch (CPU) {
-       case '' 
-    }    
 }
+
+async function test() {
+    let PR = await computerResponse();
+    let CPU = await computerResponse();
+    console.log(PR, CPU);
+    if (PR == CPU) {
+        return "It's a tie!"
+    }
+
+    console.log("test called");
+}
+
+(async () => {await test();})();
